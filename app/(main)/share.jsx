5f9055ce8ac9e334e39f862df85a9564f6e94f1a
Max-Artis-11 <max.im.enterprize@gmail.com>
2025-12-07 17:16:00 +0000

    Keep only sora-watermark-processor in Git

--- app/(main)/share.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Share() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>share</Text>
-    </View>
-  );
-}
